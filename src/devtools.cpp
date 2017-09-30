da3ef420be3509aba374d15203d63ff3c681dec8
Nick Powell <nick@maximumtwang.com>
2017-09-29 17:13:37 -0700

    Add WIP pure C API devtools generation

--- src/devtools.cpp
+++ src/devtools.cpp
@@ -63,6 +63,32 @@ c = marshal.dump(d, f)
 f.close()
 )";
 
+// WIP may never finish
+bool MakeDevtoolsNoScript(const char *devtools_file) {
+    PyObject *main = PyImport_AddModule("__main__");
+    PyObject *globals = PyDict_New();
+    PyObject *locals = PyDict_New();
+    FILE *f = fopen(devtools_file);
+    if (f == NULL) {
+        printf(" ERROR | Couldn't find devtools file\n");
+        return false;
+    }
+
+    PyRun_FileExFlags(f, devtools_file, Py_file_input,
+                      globals, locals, true, NULL);
+
+    int k = 0;
+
+    PyObject *bootstrap = PyDict_GetItemString(locals, "Bootstrap");
+    PyObject *func_code = PyObject_GetAttrString(bootstrap, "func_code");
+    PyObject *co_consts_tuple = PyObject_GetAttrString(func_code, "co_consts");
+
+    Py_ssize_t size = PyTuple_size(co_consts_tuple);
+    PyObject *co_consts = PyList_New(size);
+
+    return true;
+}
+
 bool MakeDevtools(const char *devtools_file) {
     int ret = PyRun_SimpleString(devtools_script);
     if (ret == 0) {
--- src/devtools.h
+++ src/devtools.h
@@ -2,7 +2,9 @@
 #define __DEVTOOLS_H__
 
 #include <Python.h>
+#include <marshal.h>
 
 bool MakeDevtools(const char *devtools_file);
+bool MakeDevtoolsNoScript(const char *devtools_file);
 
 #endif
